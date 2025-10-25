a89300268a7b78c65754fcc25e2b99a2db9942bd
Wish <wishyt@proton.me>
2025-10-25 10:12:51 -0500

    implement in server

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from hashlib import sha256
 import random, string
 import os
+import linecache
 
 app = Flask(__name__)
 
@@ -18,11 +19,13 @@ def index():
 def makewarning():
     warning = request.form['message']
     sentkey = request.form['key']
+    type = request.form['type']
+    service = request.form['service']
     with open("key.txt", "r") as file:
         key = file.read()
     if sentkey == key:
         with open("eas.txt", "w") as easfile:
-            easfile.write(warning)
+            easfile.write(f"{warning}\n{type}\n{service}")
         if os.path.exists("key.txt"):
             os.remove("key.txt")
         return "EAS warning sent!"
@@ -33,9 +36,24 @@ def getwarning():
     if not os.path.exists("eas.txt"):
         warning = "No EAS warning is active."
     else:
-        with open("eas.txt", "r") as file:
-            warning = file.read()
+        warning = linecache.getline('eas.txt', 1).rstrip('\n')
     return warning
+
+@app.route("/api/gettype", methods=["GET"])
+def gettype():
+    if not os.path.exists("eas.txt"):
+        type = "No EAS warning is active."
+    else:
+        type = linecache.getline('eas.txt', 2).rstrip('\n')
+    return type
+
+@app.route("/api/getmessenger", methods=["GET"])
+def getmessenger():
+    if not os.path.exists("eas.txt"):
+        messenger = "No EAS warning is active."
+    else:
+        messenger = linecache.getline('eas.txt', 3).rstrip('\n')
+    return messenger
 
 @app.route("/api/login", methods=["POST"])
 def login():
