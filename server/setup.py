56456b3db1955e59a8a885da3c70221575c938bb
Wish <wishyt@proton.me>
2025-10-24 19:52:52 -0500

    fix

--- server/setup.py
+++ server/setup.py
@@ -4,4 +4,4 @@ print("What password would you like to set for the controller?")
 password = input()
 encodedpw = sha256(password.encode('utf-8')).hexdigest()
 with open("password.txt", "w") as file:
-    file.write(f"password={encodedpw}")
+    file.write(f"{encodedpw}")
