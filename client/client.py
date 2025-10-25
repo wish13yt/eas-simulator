30f4c430ca50c01c28c5f7e3f19ff0f18f7f0f63
Wish <wishyt@proton.me>
2025-10-25 09:12:29 -0500

    update gitignore

--- client/client.py
+++ client/client.py
@@ -1,12 +1,20 @@
 import requests
 import time
+import os
 print("Welcome to EAS Simulator Client!")
 print("NOTICE: This is SIMULATED. No alerts are real. This is SIMULATED.")
 print("Licensed under Unlicense.")
 print("What URL would you like to use (ex: https://example.com)")
-url = input("")
-if not url.endswith("/"):
-    url = url + "/"
+if os.path.exists("escurl.txt"):
+    with open("escurl.txt", "r") as file:
+        urlff = file.read()
+        url = input(urlff) or urlff
+else:
+    url = input("")
+    if not url.endswith("/"):
+        url = url + "/"
+    with open("escurl.txt", "w") as file:
+        file.write(url)
 print("Great! You are set for alerts from " + url + "!")
 while True:
     time.sleep(5)
